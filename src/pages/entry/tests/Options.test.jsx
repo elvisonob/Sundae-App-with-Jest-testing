9743867677d4d63d933aaa92612969309e83de12
elvisonob <elvisonob@yahoo.com>
2023-08-10 18:36:45 +0100

    adding components to test

---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+
+import Options from '../Options';
+
+test('displays image for each scoop option from server', () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  const altText = scoopImages.map((element) => element.alt);
+
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
