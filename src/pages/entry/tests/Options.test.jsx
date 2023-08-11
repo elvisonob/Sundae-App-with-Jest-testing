5ccc9783b6d6e7d87f1ee635b938df58fab2bbe9
elvisonob <elvisonob@yahoo.com>
2023-08-11 04:54:19 +0100

    tests passed for second handler

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,22 @@ test('displays image for each scoop option from server', async () => {
 
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('display images for each toppings option from server', async () => {
+  //RCSE
+  render(<Options optionType="toppings" />);
+
+  const toppingImage = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+
+  expect(toppingImage).toHaveLength(3);
+
+  const imagesTitles = toppingImage.map((img) => img.alt);
+
+  expect(imagesTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
