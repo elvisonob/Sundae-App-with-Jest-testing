8a2c00c67f838b69a068a6e4cb721abc9b4f9a85
elvisonob <elvisonob@yahoo.com>
2023-08-11 03:49:09 +0100

    code and test scoop images from server

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('displays image for each scoop option from server', () => {
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   const altText = scoopImages.map((element) => element.alt);
