7581e21872abbb7a52501220a3304cafe2accc91
elvisonob <elvisonob@yahoo.com>
2023-08-17 04:35:50 +0100

    context file ready

--- src/contexts/OrderDetails.jsx
+++ src/contexts/OrderDetails.jsx
@@ -1,1 +1,54 @@
 import { createContext, useContext, useState } from 'react';
+import { pricePerItem } from '../constants/index';
+
+const OrderDetails = createContext();
+
+export function useOrderDetails() {
+  const contextValue = useContext(OrderDetails);
+
+  if (!contextValue) {
+    throw new Error(
+      'useOrderDetails must be called from within an OrderDetailsProvider'
+    );
+  }
+
+  return contextValue;
+}
+
+export function OrderDetailsProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: {},
+    toppings: {},
+  });
+
+  function updateItemCount(itemName, newItemCount, optionType) {
+    // make a copy of existing state
+    const newOptionCounts = { ...optionCounts };
+
+    // update the copy with the new information
+    newOptionCounts[optionType][itemName] = newItemCount;
+
+    //update the status with the updated copy
+    setOptionCounts(newOptionCounts);
+  }
+
+  function resetOrder() {
+    setOptionCounts({ scoops: {}, toppings: {} });
+  }
+
+  function calculateTotal(optionType) {
+    const countsArray = Object.values(optionCounts[optionType]);
+
+    const totalCount = countsArray.reduce((total, value) => total + value, 0);
+
+    return totalCount * pricePerItem[optionType];
+  }
+
+  const totals = {
+    scoops: calculateTotal('scoops'),
+    toppings: calculateTotal('toppings'),
+  };
+
+  const value = { optionCounts, totals, updateItemCount, resetOrder };
+  return <OrderDetails.Provider value={value} {...props} />;
+}
